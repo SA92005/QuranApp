348a768c858f167b3ffdf55ddd485d451789b74a
SA92005 <sa9432216@gmail.com>
2026-02-25 19:15:09 +0200

    make splash screen and animated text

---
+++ lib/screens/splash_text.dart
@@ -0,0 +1,22 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:quran_app/core/utilities/style.dart';
+
+class SplashText extends StatelessWidget {
+  const SplashText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTextStyle(
+      style: AppStyle.splashText,
+      child: AnimatedTextKit(
+        animatedTexts: [
+          TyperAnimatedText(
+            'القرآن الكريم',
+            speed: Duration(milliseconds: 300),
+          ),
+        ],
+      ),
+    );
+  }
+}
